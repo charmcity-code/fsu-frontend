76fb2a49a60568272a1972f8f7f4b2eded620cfa
Joshua Hsu <jshhsu168@gmail.com>
2025-08-05 08:05:02 -0700

    day5 first commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { useAuth } from '../context/AuthContext';
 import { loginUser } from '../api/api'; // Import your new function
 
